8d4c3d4260f5f3eef14cecec9b4173d09b857fba
Yuri Freitas <115326276+yur1freitas@users.noreply.github.com>
2026-02-22 19:42:56 -0300

    perf: rode a tarefa de importação em uma thread separada
    A importação de arquivos pode ser uma tarefa muito custosa dependendo do
    número de arquivos que estão sendo importados e isso causa um
    congelamento na interface do usuário
    
    Para evitar isso, o trabalho de importação foi dedicado a uma thread
    diferente para que permita que a interface continue funcionando enquanto
    o processo de importação acontence

--- src/xml2excel/components/ImportFilesButton.py
+++ src/xml2excel/components/ImportFilesButton.py
@@ -1,8 +1,28 @@
-from PySide6.QtCore import Slot
+from PySide6.QtCore import QThread, Slot
 from PySide6.QtWidgets import QFileDialog, QPushButton
 
-from xml2excel.commands.import_files import ImportFilesInput, import_files
+from xml2excel.commands.import_files import (
+    ImportFilesInput,
+    ImportFilesOutput,
+    import_files,
+)
 from xml2excel.components import App
+from xml2excel.utils.worker import Worker
+
+
+class ImportFilesWorker(Worker):
+    def __init__(self, input: ImportFilesInput):
+        super().__init__()
+
+        self.input = input
+
+    def run(self):
+        self.started.emit()
+
+        output = import_files(self.input)
+        self.progress.emit(output)
+
+        self.finished.emit()
 
 
 class ImportFilesButton(QPushButton):
@@ -12,11 +32,10 @@ class ImportFilesButton(QPushButton):
         super().__init__(**kwargs)
 
         self.app = App.instance()
-
-        self.clicked.connect(self._click)
+        self.clicked.connect(self._runtask)
 
     @Slot()
-    def _click(self) -> None:
+    def _runtask(self) -> None:
         store = self.app.store
         config = self.app.config
 
@@ -26,13 +45,38 @@ class ImportFilesButton(QPushButton):
         )
 
         if dirpath:
-            output = import_files(
-                ImportFilesInput(
-                    path=dirpath,
-                    recursive=config.recursive,
-                    prefix_mode=config.prefix_mode,
-                )
+            input = ImportFilesInput(
+                path=dirpath,
+                recursive=config.recursive,
+                prefix_mode=config.prefix_mode,
             )
 
-            store.data = output.data
-            store.filepaths = output.filepaths
+            self._thread = QThread()
+
+            self._worker = ImportFilesWorker(input)
+            self._worker.moveToThread(self._thread)
+
+            self._thread.started.connect(self._worker.run)
+            self._thread.finished.connect(self._thread.deleteLater)
+
+            @Slot()
+            def on_started():
+                self.setEnabled(False)
+
+            @Slot(ImportFilesOutput)
+            def on_progress(output: ImportFilesOutput):
+                store.data = output.data
+                store.filepaths = output.filepaths
+
+            @Slot()
+            def on_finished():
+                self.setEnabled(True)
+
+                self._thread.quit()
+                self._worker.deleteLater()
+
+            self._worker.started.connect(on_started)
+            self._worker.progress.connect(on_progress)
+            self._worker.finished.connect(on_finished)
+
+            self._thread.start()

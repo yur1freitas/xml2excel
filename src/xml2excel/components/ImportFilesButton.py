52cc8b8b3dae2797157803734c8101a75dafded1
Yuri Freitas <115326276+yur1freitas@users.noreply.github.com>
2026-02-22 18:07:44 -0300

    refactor: crie um contexto global através de `App`
    Anteriormente o contexto global era passado via parâmetro no construtor,
    mas há uma maneira mais simples usando a classe 'App' para armazenar os
    dados e as configurações. Assim basta usar o método que retorna a
    instância atual e acessar as propriedades nos componentes
    
    Por isso os parâmetros de contexto foram removidos de todos os
    componentes e usado 'App.instance()' para acessar as informações
    do contexto global

--- src/xml2excel/components/ImportFilesButton.py
+++ src/xml2excel/components/ImportFilesButton.py
@@ -2,23 +2,23 @@ from PySide6.QtCore import Slot
 from PySide6.QtWidgets import QFileDialog, QPushButton
 
 from xml2excel.commands.import_files import ImportFilesInput, import_files
-from xml2excel.manager.context import GlobalContext
+from xml2excel.components import App
 
 
 class ImportFilesButton(QPushButton):
     DIALOG_TITLE = 'Importar XMLs'
 
-    def __init__(self, ctx: GlobalContext, **kwargs):
+    def __init__(self, **kwargs):
         super().__init__(**kwargs)
 
-        self._ctx = ctx
+        self.app = App.instance()
 
         self.clicked.connect(self._click)
 
     @Slot()
     def _click(self) -> None:
-        store = self._ctx.store
-        config = self._ctx.config
+        store = self.app.store
+        config = self.app.config
 
         dirpath = QFileDialog.getExistingDirectory(
             self,

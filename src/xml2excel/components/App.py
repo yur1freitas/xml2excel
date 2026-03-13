f357c4b02e0c58a9364b1445fb78d416c8e9cf70
Yuri Freitas <115326276+yur1freitas@users.noreply.github.com>
2026-03-13 17:02:25 -0300

    feat: crie um novo tema para a aplicação
    O tema usado anteriormente era provido pelo pacote 'qt-material' que
    serviu como uma solução temporária, mas o tema não possui um aspecto
    moderno e não é muito atraente
    
    Então foi criado um novo tema baseado no visual do ShadCN e na forma que
    o 'qt-material' funciona. Além disso o tema possui cores para modo
    escuro e claro que poderá ser alternado dinamicamente em uma
    funcionalidade futura

--- src/xml2excel/components/App.py
+++ src/xml2excel/components/App.py
@@ -2,13 +2,16 @@ from typing import cast
 
 from PySide6.QtWidgets import QApplication
 
+from xml2excel.aliases import AnyPath
 from xml2excel.manager.config import Config
 from xml2excel.manager.store import Store
+from xml2excel.theme import Theme
 
 
 class App(QApplication):
     config = Config()
     store = Store()
+    theme = Theme()
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -16,3 +19,9 @@ class App(QApplication):
     @staticmethod
     def instance() -> 'App':
         return cast(App, QApplication.instance())
+
+    def load_stylesheet(self, path: AnyPath):
+        stylesheet = self.theme.read_stylesheet(path)
+
+        if stylesheet:
+            self.setStyleSheet(stylesheet)
